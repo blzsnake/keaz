a388caa304db9fd2fe573d6af5ccef309cce375b
boldyrevap <boldyrevap@gmail.com>
2015-11-05 23:27:49 +0300

    footer: adaptive styles and little bit JS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,10 +4,29 @@ import 'slick-carousel';
 import '../blocks/main-slider/main-slider';
 import '../blocks/new-slider/new-slider';
 
-
 $(() => {
 	// Code here
-	$('.footer__mobile').on('click', function(){
-		$(this).next('div').fadeToggle(500);
+
+
+	$(document).ready(function (){	
+
+		$('.footer__mobile').on('click', function(){
+			$(this).next('div').fadeToggle(500).css('display', 'inline-block');
+		});
+
+
+		$(window).resize(function(){
+			var winBr = $(window).width();
+			if(winBr>600){
+				$('div.footer__list').css('display', 'inline-block')
+			}
+			else{
+				$('div.footer__list').css('display', 'none')
+			}
+		});     
+
 	});
+
+
+	
 });
