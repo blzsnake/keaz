182cf71c7c974ad94acb0a17bad595c74a5861b0
blzsnake <kurskatm@gmail.com>
2015-11-18 13:05:38 +0300

    feat drop popup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,18 +7,21 @@ import '../blocks/new-slider/new-slider';
 
 $(() => {
 'use strict';
+
+	var	active = 'is-active';
+
 	$('.js-filter-option, .js-filter-title').on('click', function() {
-		$(this).toggleClass('is-active');
+		$(this).toggleClass(active);
 	});
 
 	$('.js-toggl').on('click', function(e) {
-		$(this).closest('.product-toggl').toggleClass('is-active');
+		$(this).closest('.product-toggl').toggleClass(active);
 	});
 
 	$('.js-switch').on('click', function () {
 		var $this = $(this),
 			$tabs = $('.tabs__container'),
-			active = 'is-active';
+			active = active;
 		if ($this.hasClass(active)) {
 			$this.removeClass(active);
 			$tabs.removeClass(active);
@@ -35,8 +38,7 @@ $(() => {
 		var $this = $(this),
 			$lists = $('.catalog-list,.catalog-tile'),
 			$list = $('.catalog-list'),
-			$tile = $('.catalog-tile'),
-			active = 'is-active';
+			$tile = $('.catalog-tile');
 			console.log($lists, $list, $tile);
 		if ($this.hasClass(active)) {
 			$this.removeClass(active);
@@ -50,6 +52,17 @@ $(() => {
 			} else {
 				$tile.addClass(active)
 			}
+		}
+	});
+
+	$('.js-dropdown').on('click', function() {
+		var $this = $(this);
+
+		if ($this.hasClass(active)) {
+			$this.removeClass(active);
+		} else {
+			$('.js-dropdown').removeClass(active);
+			$(this).addClass(active);
 		}
 	});
 
